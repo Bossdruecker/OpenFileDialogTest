968b97f043f6823ecabf62b36006ec0c9396a4a5
Daniel <bossdruecker@gmail.com>
2019-02-18 20:52:47 +0100

    First try

---
+++ OpenFileDialog/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Collections;
+
+namespace OpenFileDialogTest
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filePath = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            if(openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                Process.Start(filePath);
+            }
+        }
+    }
+}
